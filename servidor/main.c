8e06ec31a70069244753bfd0cf46c261631638ee
juaneem-cmyk <juaneem@ciencias.unam.mx>
2026-09-08 13:28:24 -0600

    Comienzo

--- servidor/main.c
+++ servidor/main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-void cuboPorReferencia( int *Num);
+/*Voy a seguir un tutorial.
+ https://medium.com/@trish07/building-a-simple-tcp-chat-application-in-c-a-step-by-step-tutorial-ed3845607d16 */
 
-int main(){
-    int numero = 5;
-    printf("El valor original del numero es %d", numero);
-    cuboPorReferencia(&numero);
-    printf ("\nEl nuevo valor de número es %d\n", numero);
-    return 0;
-}
-void cuboPorReferencia(int *Num){
-    *Num = (*Num) * (*Num) * (*Num);
-}
+#define PUERTO 1234
+#define BUFFER_SIZE 1024
