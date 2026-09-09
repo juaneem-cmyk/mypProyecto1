516f816f441a038cce556bbd73f08505e78cbf8b
juaneem-cmyk <juaneem@ciencias.unam.mx>
2026-09-08 20:09:47 -0600

    sockets

--- servidor/main.c
+++ servidor/main.c
@@ -17,5 +17,30 @@ int main() {
     struct sockaddr_in direccion;
     int opcion=1;
     char buffer[BUFFER_SIZE]= {0};
+    // Creo el socket del servidor
+    if ((servidor_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        perror("Error al crear el socket");
+        exit(EXIT_FAILURE);
+    }
+    // Configuro el socket para que pueda reutilizar la dirección y el puerto
+    if (setsockopt(servidor_socket, SOL_SOCKET, SO_REUSEADDR, & opcion, sizeof(opcion)) == -1) {
+        perror("Error al configurar el socket");
+        exit(EXIT_FAILURE);
+    }
+    // Configuro la dirección del servidor
+    direccion.sin_family = AF_INET;
+    direccion.sin_addr.s_addr = INADDR_ANY;
+    direccion.sin_port = htons(PUERTO);
+    // Asigno la dirección al socket
+    if (bind(servidor_socket, (struct sockaddr *) & direccion, sizeof(direccion)) < 0) {
+        perror("Error al asignar la dirección al socket");
+        exit(EXIT_FAILURE);
+    }
+    // Escucho conexiones entrantes
+    if (listen(servidor_socket, 3) < 0) {
+        perror("Error al escuchar conexiones entrantes");
+        exit(EXIT_FAILURE);
+    }
+    printf("Servidor escuchando en el puerto %d\n", PUERTO);
     return 0;
 }
