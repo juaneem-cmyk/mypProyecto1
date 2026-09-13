10625bc35e55a7e94a54caeb9104e471dcf44468
juaneem-cmyk <juaneem@ciencias.unam.mx>
2026-09-12 21:40:12 -0600

    comienza el usuario de a devis

---
+++ cliente/cliente.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+/* Encontré 2 repositorios de referencia y un libro que habla de un proyecto multiusuario de chat 
+https://beej.us/guide/bgnet0/
+https://github.com/AmineToualbi/TCPChat.git
+https://github.com/BaseMax/TCP-Chat-CPP.git
+*/
---
+++ cliente/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/*Esto lo iba a colocar en el reporte pero supongo que aquí también es importante,
+  estoy leyendo un libro que se llama "Como programar en C/C++ y Java" de la editorial Prentice Hall 
+  tiene unas hormigas en la portada :D*/
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
