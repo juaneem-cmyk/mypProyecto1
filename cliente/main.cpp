cadfe6c4e15df5d42ff2f3ec9c6cd6c3e7b90763
juaneem-cmyk <juaneem@ciencias.unam.mx>
2026-09-15 14:16:48 -0600

    Cliente en C++

--- cliente/main.cpp
+++ cliente/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include "TCPCliente.h"
 
 /*Esto lo iba a colocar en el reporte pero supongo que aquí también es importante,
   estoy leyendo un libro que se llama "Como programar en C/C++ y Java" de la editorial Prentice Hall 
   tiene unas hormigas en la portada :D*/
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    TCPCliente cliente("127.0.0.1", 1234);
+
+    if (!cliente.conectar()) {
+      return 1;
+    }
+    std::cout << "Cliente conectado al servidor." << std::endl;
     return 0;
 }
